f78f3ae346bf3d7bb4064915006ce5f44adcee67
shiva prasad <shiva@secondsdigital.com>
2020-03-13 23:45:57 +0530

    ini bug

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ else
 switch (ENVIRONMENT)
 {
 	case 'development':
-	ni_set('display_errors', 1);
+	ini_set('display_errors', 1);
 		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
 		//ini_set('display_errors', 1);
 	break;
