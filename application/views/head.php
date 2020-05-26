7e40e42e1c85c17b55360000b07beac72483b279
shiva prasad <shiva@secondsdigital.com>
2020-05-26 12:32:09 +0530

    changes in index test

--- application/views/head.php
+++ application/views/head.php
@@ -96,6 +96,24 @@
     list-style: none;
 }
         </style>
+        <!-- Taboola Pixel Code -->
+<script type='text/javascript'>
+  window._tfa = window._tfa || [];
+  window._tfa.push({notify: 'event', name: 'page_view', id: 1302409});
+  !function (t, f, a, x) {
+         if (!document.getElementById(x)) {
+            t.async = 1;t.src = a;t.id=x;f.parentNode.insertBefore(t, f);
+         }
+  }(document.createElement('script'),
+  document.getElementsByTagName('script')[0],
+  '//cdn.taboola.com/libtrc/unip/1302409/tfa.js',
+  'tb_tfa_script');
+</script>
+<noscript>
+  <img src='https://trc.taboola.com/1302409/log/3/unip?en=page_view'
+      width='0' height='0' style='display:none'/>
+</noscript>
+<!-- End of Taboola Pixel Code -->
     </head>
     <body class="abcd">
         <!-- Google Tag Manager (noscript) -->
--- application/views/indexbackup223052020.php
+++ application/views/indexbackup223052020.php
@@ -75,7 +75,7 @@ height: 70%;
 }
 @media (max-width: 414px) {
 .city-section{
-    margin-top: -22px;
+    margin-top: -100px;
 }
 }
  
@@ -90,18 +90,96 @@ $(document).ready(function(){
 </script>
 
 
-<div id="carousel" class="carousel slide" data-ride="carousel">
-<!-- <iframe width="100%" height="600" src="https://www.youtube.com/embed/h3hRUAu_UJo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-<video width="100%" height="auto" controls autoplay muted="">
-  <source src="<?=base_url("assets/landmark.mp4");?>" type="video/mp4"> 
-</video>
-    </div>
+<div id="carousel" class="carousel slide visible-lg-block visible-xs-block  hidden-lg" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
 
+    <dAiv class="carousel-inner ">
+        <?php $i = 0;
+        foreach ($mobilesliders as $slider) {
+            $img=array();
+            $img=explode('.', $slider->image);
+           // print_r($img);
+          // $mobile_banner=$img[0].'_thumb.jpg';
+        ?>
+            <div class="<?= $i ? '' : 'active' ?> item fadeInRight animated"
+                 style="background: url(<?= base_url('uploads/sliders/' .  $slider->image) ?>);">
+                 <div class="carousel-caption fadeInUp animated">
+                    <h1><b><?= $slider->title ?></b></h1>
+                    <p><?= $slider->heading ?></p>
+                 </div>
+            </div>
+            <?php $i++;
+        } ?>
+    </div>
+    <!-- Left and right controls -->
+    <a class="left mobile carousel-control" href="#carousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right mobile carousel-control" href="#carousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
 </div>
 
 
+     <!-- <div id="carousel" class="carousel slide home-slider hidden-xs-block hidden-xs " data-ride="carousel">
+        <div class="carousel-inner">
+            <?php $i = 0;
+            foreach ($sliders as $slider) {
+            ?>
+            
+                <div class="<?= $i ? '' : 'active'  ?> item fadeInRight animated img-responsive"
+                    style="100%!important; background: url(<?= base_url('uploads/sliders/' . $slider->image) ?>);">
+                    <div class="carousel-caption fadeInUp animated">
+                        <h1><b><?= $slider->title ?></b></h1>
+                        <p><?= $slider->heading ?></p>
+                    </div>
+                </div>
+                <?php $i++;
+            } ?>
+        </div>
+    </div>  -->
 
+    <div id="myCarousel" class="carousel slide hidden-xs-block hidden-xs" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <div class="carousel-inner">
+    <?php $desk = 0;
+        foreach ($sliders as $slider) {
+            $img=array();
+            $img=explode('.', $slider->image);
+           
+        ?>
+      <div class="<?= $desk ? 'item' : 'item active' ?>">
+      <img src="<?= base_url('uploads/sliders/' .  $slider->image) ?>" style="width: 100%!important;" alt="slider">
+      </div>
+       <?php $desk++;
+        
+            } ?>
+
+    </div>
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 </div>
 <div class="clearflix"></div>
 
