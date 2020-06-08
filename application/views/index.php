917965fc3f5b1cadeb557e6e6ab2c6c7cee45f1f
preetika <shiva@seconddigital.com>
2020-06-08 12:14:19 +0530

    css modify for smallest screen 320px

--- application/views/index.php
+++ application/views/index.php
@@ -442,7 +442,7 @@ $(document).ready(function(){
 
 <!-- Tabs start-->
 <div class="container tabbable-panel ">
-    <div class="d-banner">
+    <div class="">
   <ul class="nav nav-pills nav-justified">
     <li class="blue"><a  href="<?=base_url('city/Bangalore');?>" target="_blank">Bangalore</a></li>
     <li class="blue1"><a  href="<?=base_url('city/Pune');?>">Pune</a></li>
@@ -463,17 +463,6 @@ $(document).ready(function(){
   </div>
   </div>
 
-  <div class="m-banner">
-  <ul class="nav nav-pills nav-justified">
-    <li class="blue"><a  href="<?=base_url('city/Bangalore');?>" target="_blank">Bangalore</a></li>
-    <li class="blue1"><a  href="<?=base_url('city/Pune');?>">Pune</a></li>
-    <li class="blue2"><a  href="<?=base_url('city/Hyderabad');?>">Hyderabad</a></li>
-    <li class="blue3"><a  href="<?=base_url('city/Mumbai');?>">Mumbai</a></li>
-    <li class="blue4"><a href="<?=base_url('city/Noida');?>">Noida</a></li>
- 
-  </ul>
-
-  </div>
 
   <script>
     function appendcity(city)
