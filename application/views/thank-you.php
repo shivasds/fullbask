5e02fe2b8c86b0be0ba1e4b7b848b44de0075635
shiva prasad <shiva@secondsdigital.com>
2020-07-11 18:50:21 +0530

    Thanks pic commented

--- application/views/thank-you.php
+++ application/views/thank-you.php
@@ -65,10 +65,10 @@ if (!defined('BASEPATH'))
 
 <div class="container">
     <div class="thank_wrap text-center">
-        <div class="tpic">
+        <!-- <div class="tpic">
             <img src="assets/img/thankpic.png" alt="">
         </div>
-        <h3 class="thank_head">Thank you for your interest<?= ($this->input->get('title')) ? ' in '.$this->input->get('title').'!' : '!' ?></h3>
+ -->        <h3 class="thank_head">Thank you for your interest<?= ($this->input->get('title')) ? ' in '.$this->input->get('title').'!' : '!' ?></h3>
         <div class="th2">
             For immediate Assistance  <span class="phoneno"><a href="tel:+919019011888" style="text-decoration:none;color:#333">+919019011888</a></span>
         </div>
