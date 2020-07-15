de77ffd7aafe35928953e45abcc50e999534ef25
shiva prasad <shiva@secondsdigital.com>
2020-07-15 22:27:58 +0530

    Resolved 404 images

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -26,3 +26,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $base  = "https://".$_SERVER['HTTP_HOST'];
 $base .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $base;
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_()@&\-!';
